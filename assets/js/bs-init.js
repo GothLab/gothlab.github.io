baa5703bbaf7849bad88aa7debab48e80eafd9ec
GothLab <53623054+GothLab@users.noreply.github.com>
2023-04-22 16:12:32 +0300

    ert

--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -1,10 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-	var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bss-tooltip]'));
-	var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-	  return new bootstrap.Tooltip(tooltipTriggerEl);
-	})
-
 	var toastTriggers = document.querySelectorAll('[data-bs-toggle="toast"]');
 
 	for (let toastTrigger of toastTriggers) {
--- assets/js/navclick.js
+++ assets/js/navclick.js
@@ -1,3 +1,18 @@
 /*$('#vfx .TRGR').on('mouseover',function(){
 $(this).click();
 })*/
+
+function hider () {
+    
+    $('#VFX-1').hide();
+    $('#PACKS').hide();
+    
+};
+
+function show (x) {
+  $('#'+x).show();  
+    
+};
+
+hider();show('PACKS');
+$('#bpacks').click();
