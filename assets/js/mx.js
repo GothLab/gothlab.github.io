04d1c64be203d8f375054e1d20d3567fb57c060a
GothLab <53623054+GothLab@users.noreply.github.com>
2023-11-03 15:27:46 +0300

    d

--- assets/js/mx.js
+++ assets/js/mx.js
@@ -2,7 +2,7 @@
 
             var mixer = mixitup(containerEl, {
                 multifilter: {
-                    enable: true
+                    enable: false
                 },
                
     "animation": {
