c19e64ab700039d70158775d91fe6244f2697d37
GothLab <53623054+GothLab@users.noreply.github.com>
2023-12-29 20:15:05 +0300

    s

--- assets/js/extra.js
+++ assets/js/extra.js
@@ -3,7 +3,7 @@ $('#beffects').on('click',function(){
 	
 	$("#VFX-1").show();
 	$("#mixer").hide();
-	$()
+	
 });
 
 $('#bpacks').on('click',function(){
@@ -11,7 +11,20 @@ $('#bpacks').on('click',function(){
 	
 	$("#VFX-1").hide();
 	$("#mixer").show();
-	$()
+	
 });
 
 $("#VFX-1").hide();
+
+
+
+
+  $(document).ready(function() {
+    // Custom jQuery filter for case-insensitive :contains
+    $.expr[':'].containsIgnoreCase = function (n, i, m) {
+      return jQuery(n).text().toUpperCase().indexOf(m[3].toUpperCase()) >= 0;
+    };
+
+    // Find all .card-title elements that contain the text "free" (case-insensitive) and add CSS
+    $('.card-title:containsIgnoreCase("free")').css('color', '#42bedf').text('Free');
+  });
